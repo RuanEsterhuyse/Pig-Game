09bb15cc2abe3408dfb2ee7e54d98ae8f248c74f
Ruan Esterhuyse <ruan.esterhuyse@gmail.com>
2022-01-18 08:39:20 +0200

    select elements

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const score0El = document.querySelector('#score--0');
+const score1EL = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice'); 
+
+score0El.textContent = 0;
+score1EL.textContent = 0;
+diceEl.classList.add('hidden');
